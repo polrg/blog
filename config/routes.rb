f2b03c58ee7ccacefc86e8a66558f3847e71a9bd
polrg <pascal.olivier.1@gmail.com>
2022-10-13 21:20:13 +0200

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index" # URL /articles skickas till action INDEX i controller ARTICLES
-  get "/articles/:id", to: "articles#show" # URL /articles/186  skickas till action SHOW och 186 till params[:id]
+=begin   get "/articles", to: "articles#index" # URL /articles skickas till action INDEX i controller ARTICLES
+  get "/articles/:id", to: "articles#show" # URL /articles/186  skickas till action SHOW och 186 till params[:id] 
+=end
+  resources :articles
 
 end
