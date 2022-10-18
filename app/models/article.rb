10398a10f8a5f04f1a26457042a20fd54fe2eb62
polrg <pascal.olivier@utb.salem.se>
2022-10-18 11:47:02 +0200

    1

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     include Visible
-    has_many :comments
+    has_many :comments, dependent: :destroy  # sista gör att alla dependant comments delete om article deletes
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
 
